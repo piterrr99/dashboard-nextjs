c6448a36b9c5e21213c735ccd14efe5380e93f7e
Pedro Hourrutiner <pedrodavidhourrutinerdiaz@gmail.com>
2025-03-04 19:47:56 -0500

    test14

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: '/dashboard-nextjs-cik94gmy9-pedro-hourrutiners-projects.vercel.app'
 };
 
 module.exports = nextConfig;
